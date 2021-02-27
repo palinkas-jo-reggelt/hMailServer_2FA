4544df22eef5a24e28eeaeb7570990a0f3b430b0
palinkas-jo-reggelt <52140617+palinkas-jo-reggelt@users.noreply.github.com>
2021-02-27 10:44:53 -0500

    Update head.php

--- head.php
+++ head.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html> 
 <html>
 <head>
-<title>wap.dynu.net SMS Password Changer</title>
+<title>Password Changer</title>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <meta http-equiv="Content-Style-Type" content="text/css">
 <meta name="viewport" content="width=device-width, initial-scale=1">
